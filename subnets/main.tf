fafe65f7bcfd63fa3b1cef7efa120275053f9d9c
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-19 11:50:51 -0400

    Terraform Script

--- subnets/main.tf
+++ subnets/main.tf
@@ -8,5 +8,10 @@ resource "aws_subnet" "main" {
   }
 }
 
-variable "subnets" {}
-variable "vpc_id" {}
+resource "aws_route_table" "main" {
+  for_each = var.subnets
+  vpc_id = var.vpc_id
+  tags = {
+    Name = each.key
+  }
+}
