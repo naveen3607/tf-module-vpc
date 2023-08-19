d93b705d383e12f11f540a3d146126ba9443ca1a
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-19 13:56:49 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_route" "igw" {
 }
 
 resource "aws_eip" "ngw" {
-  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  count = local.public_subnet_ids
   domain   = "vpc"
 }
 
