aed24a4032ccfc0ca597ac81d5478c11628acd87
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-19 13:58:16 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -25,18 +25,18 @@ resource "aws_route" "igw" {
 }
 
 resource "aws_eip" "ngw" {
-  count = local.public_subnet_ids
+  count = length(local.public_subnet_ids)
   domain   = "vpc"
 }
 
 resource "aws_nat_gateway" "ngw" {
-  count = local.public_route_table_ids
+  count = length(local.public_route_table_ids)
   allocation_id = element(aws_eip.ngw.*.id, count.index)
   subnet_id     = element(local.public_route_table_ids, count.index)
 }
 
 resource "aws_route" "ngw" {
-  count = local.private_route_table_ids
+  count = length(local.private_route_table_ids)
   route_table_id = element(local.private_route_table_ids, count.index)
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = element(aws_nat_gateway.ngw.*.id, count.index)
